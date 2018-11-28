d5a4a839b7e48fdae3eabe077cd30cf7413b333f
wtartanus <wtartanus@gmail.com>
2018-11-28 10:32:32 +0100

    Added swipe functionality

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -21,6 +21,17 @@ export class CarouselItemElementDirective {
 }
 
 const EASEIN = '250ms ease-in';
+interface PositionDifferences {
+  x: number;
+  y: number;
+}
+
+enum Swipe_Type {
+  Left,
+  Right,
+  Up,
+  Down
+}
 
 @Component({
   selector: 'app-carousel',
@@ -38,6 +49,8 @@ export class CarouselComponent implements AfterViewInit {
   private player: AnimationPlayer;
   private itemWidth: number;
   private currentSlide = 0;
+  private initialX: number;
+  private initialY: number;
   carouselWrapperStyle = {};
 
   constructor( private builder: AnimationBuilder ) {}
@@ -51,6 +64,70 @@ export class CarouselComponent implements AfterViewInit {
     this.carouselWrapperStyle = {
       width: `${this.itemWidth}px`
     };
+  }
+
+  startTouchEvent(event) {
+    const { clientX, clientY } = event.touches[0];
+    this.setInitialPosition(clientX, clientY);
+  }
+
+  private setInitialPosition(x: number, y: number) {
+    this.initialX = x;
+    this.initialY = y;
+  }
+
+  onTouchMove(event) {
+    event.preventDefault();
+    try {
+      this.throwIfInitalPositionsNotPresent();
+
+      const { clientX, clientY } = event.touches[0];
+      const differences = this.getDifferences(clientX, clientY);
+      const swipeDirection = this.getSwipeDirection(differences);
+      this.proccessSwipe(swipeDirection);
+    } catch (error) {
+      // Just do nothing if initial positions arent set
+    }
+
+    this.setInitialPosition(null, null);
+  }
+
+  private throwIfInitalPositionsNotPresent(): any {
+    if (this.initialX == null || this.initialY == null) {
+       throw new Error('Postion inital values not provided');
+    }
+  }
+
+  private getDifferences(x, y): PositionDifferences {
+    return {
+      x: this.initialX - x,
+      y: this.initialY - y
+    };
+  }
+
+  private getSwipeDirection(movePositions: PositionDifferences): Swipe_Type {
+    if (this.isHorizontalMove(movePositions)) {
+      return movePositions.x > 0 ? Swipe_Type.Left : Swipe_Type.Right;
+    } else {
+      return movePositions.x > 0 ? Swipe_Type.Up : Swipe_Type.Down;
+    }
+  }
+
+  private isHorizontalMove(movePositions: PositionDifferences): boolean {
+    return Math.abs(movePositions.x) > Math.abs(movePositions.y);
+  }
+
+  private proccessSwipe(direction: Swipe_Type) {
+    switch (direction) {
+      case Swipe_Type.Left:
+        this.next();
+        break;
+      case Swipe_Type.Right:
+        this.prev();
+        break;
+      default:
+        // Fell trhought
+    }
   }
 
   next() {
